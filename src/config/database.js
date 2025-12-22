002e599165d2e183fe8cc6ab05ccbd5bc94d25d7
Shaeranw01 <sharan.w01@gmail.com>
2025-12-22 08:13:57 -0800

    .env file fixed

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
   await mongoose.connect(process.env.MONGODB_URL);
